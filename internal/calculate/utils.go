ce01b7beb15b89cb4c1f5091df18703a77b74b91
kiselevos <olegsergeevichkiselev@gmail.com>
2025-10-14 20:51:19 +0800

    fix bag with validator & add descripton to calculate

--- internal/calculate/utils.go
+++ internal/calculate/utils.go
@@ -28,7 +28,7 @@ func ValidateCalculateInput(input CalculateInput) error {
 
 // validateCoefficient проверяет, что коэффициент от 100 до 200 с шагом 10.
 func validateCoefficient(coeff uint64) bool {
-	return coeff >= 100 && coeff <= 200 && (coeff-100)%10 == 0
+	return coeff >= 100 && coeff <= 200 && (coeff-100)%5 == 0
 }
 
 // GetStartMonth возвращает номер месяца из переданной даты.
