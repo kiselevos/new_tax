45e19658ce2d1453e7f827e63bba585ecf4e2faa
kiselevos <olegsergeevichkiselev@gmail.com>
2025-12-12 15:17:07 +0800

    change Unary logger

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -19,13 +19,13 @@ import (
 
 const requestIDKey = "x-request-id"
 
-func UnaryLogger(base *slog.Logger) grpc.UnaryServerInterceptor {
+func UnaryLogger() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 		// request id
 		rid := newRID(ctx)
 
 		// базовый логгер
-		logger := base.With("method", info.FullMethod, "rid", rid)
+		logger := slog.Default().With("method", info.FullMethod, "rid", rid)
 
 		// peer ip (без порта)
 		if p, ok := peer.FromContext(ctx); ok && p != nil && p.Addr != nil {
